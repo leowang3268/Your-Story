24618bcb2ea5c8d5f9c0d4b9067f8c000048076e
leowang <leowang3268@gmail.com>
2022-01-01 13:52:17 +0800

    try senior's code

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ if channel_access_token is None:
 line_bot_api = LineBotApi(channel_access_token)
 parser = WebhookParser(channel_secret)
 
-
+'''
 @app.route("/callback", methods=["POST"])
 def callback():
     signature = request.headers["X-Line-Signature"]
@@ -79,6 +79,7 @@ def callback():
         )
 
     return "OK"
+'''
 
 
 @app.route("/webhook", methods=["POST"])
