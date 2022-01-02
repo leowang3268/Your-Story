c851acf082e09bd2054c1fd88c2bb86fcc4df9a6
leowang <leowang3268@gmail.com>
2022-01-02 20:33:54 +0800

    test. Ver 2

--- app.py
+++ app.py
@@ -213,9 +213,9 @@ def callback():
         if not isinstance(event.message, TextMessage):
             continue
 
-        line_bot_api.reply_message(
-            event.reply_token, TextSendMessage(text=event.message.text)
-        )
+        # line_bot_api.reply_message(
+        #     event.reply_token, TextSendMessage(text=event.message.text)
+        # )
 
     return "OK"
 
--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage,  Template
 
 
 channel_access_token = os.getenv(
-    "LINE_CHANNEL_ACCESS_TOKEN", "Qz7htfBX3N2xPvqqlFCjD/k5sANOj33rgXp/4tNVVFTPUry1BFvQfrvw8dEEzsPp4yTSFtgNkP84seW6BUb8m7kvy1qDVjslDa2pfamLvc6TtS1YuTWYC2rhbyivUNqap8WijQZ5NoMhKJGeV6Tf5AdB04t89/1O/w1cDnyilFU=")
+    "LINE_CHANNEL_ACCESS_TOKEN", None)
 
 
 def send_text_message(reply_token, text):
